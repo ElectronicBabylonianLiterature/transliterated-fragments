f72d45e57238fa0993a4f8da77e4665497da66c3
Yunus <yunus.cobanoglu@yahoo.de>
2023-10-17 14:27:51 +0200

    parallel processing

--- ebl/example.py
+++ ebl/example.py
@@ -1,4 +1,5 @@
 import json
+from multiprocessing import Pool
 from pathlib import Path
 
 import requests
@@ -33,24 +34,23 @@ def get_first_batch_fragments(url):
     fragments = data["fragments"]
     return fragments
 
-
-
-def get_some_fragments_and_parse(url):
-    url_string = url(0)
-    resp = requests.get(url_string)
-    data = resp.json()
-    fragments = data["fragments"]
-    for fragment in tqdm(fragments):
-        atf = fragment["atf"]
-        parsed_atf = parse_atf_lark(atf)
-        fragment["text"] = parsed_atf
+def parse_fragment(fragment):
+    atf = fragment["atf"]
+    parsed_atf = parse_atf_lark(atf)
+    fragment["text"] = parsed_atf
+    return fragment
+def parse_fragments(fragments):
+    with Pool(processes=None) as p:
+        fragments = list(tqdm(p.imap(parse_fragment, fragments), total=len(fragments)))
     return fragments
 
 
 
 if __name__ == "__main__":
-    url = lambda skip: f"http://0.0.0.0:8000/fragments/retrieve-all?skip={skip}"
-    fragments = get_all_fragments(url)
-    #fragments = get_first_batch_fragments(url)
+    URL = "http://0.0.0.0:8000/fragments/retrieve-all"
+    url = lambda skip: f"{URL}?skip={skip}"
+    #fragments = get_all_fragments(url)
+    fragments = get_first_batch_fragments(url)
+    fragments = parse_fragments(fragments)
     with open(Path(__file__).parent / "fragments.json", "w") as f:
         json.dump(fragments, f)
